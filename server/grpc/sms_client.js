dd9704ac5b7c6e0604dd92da4a93cd22aed34824
Drake Witt <dwitt@dranweb.com>
2019-10-26 16:33:19 -0400

    Invitation via web url works

--- server/grpc/sms_client.js
+++ server/grpc/sms_client.js
@@ -23,14 +23,15 @@ function createGroupInvite(messageBodyContent, phoneNumberList) {
   const request = new messages.GroupInvite();
   request.setInvitePhoneNumList(phoneNumberList);
   request.setBody(messageBodyContent);
-  client.createGroupInvite(request, (err, response) => {
-    // right now the response is not being handled.
-    // TODO(Drake): Actually fucking implement it.
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(response.getInvitesList());
+  return new Promise((resolve, reject) => {
+    client.createGroupInvite(request, (err, response) => {
+      // right now the response is not being handled.
+      // TODO(Drake): Actually fucking implement it.
+      if (err) {
+        reject(err);
+      }
+      resolve(response.getInvitesList());
+    });
   });
 }
 
--- server/routes/invite.js
+++ server/routes/invite.js
@@ -14,7 +14,7 @@ const router = express.Router();
  * The POST body must contain a list of phone numbers in the specified format.
  */
 router.post('/', (req, res) => {
-  if (!req.user) {
+  if (req.user) {
     res.json({ error: 'Not authorized' });
   } else if (!req.body.phoneNumbers) {
     res.status(400).json({ error: 'Request body missing phone numbers.' });
@@ -22,7 +22,11 @@ router.post('/', (req, res) => {
     // TODO: Validate the phone numbers are in the following format: +11234567890
     // TODO: Include the user doing the inviting in the message body.
     // TODO: Also make the message body not a fucking meme.
-    sms.createGroupInvite('', req.body.phoneNumbers);
+    sms.createGroupInvite('Ape Benison invited you to join unknown product name #69! Join'
+      + ' using this link: ', JSON.parse(req.body.phoneNumbers)).then((response) => {
+      // TODO: Store the token and expiration in Mongo
+      res.status(200).json({ message: response });
+    });
   }
 });
 
