5c31055fef0fde2cb981c71ea9be44316ee3fd08
Andreas Paljug <andreaspaljug@gmail.com>
2019-10-26 11:49:45 -0400

    added genre's to histogram

--- server/services/grouping.js
+++ server/services/grouping.js
@@ -5,13 +5,20 @@ const spotify = require('../services/spotify')
 const getValues = (user, done) => {
     const userArtists = [];
     const userTracks = [];
+    const genres = [];
     const out = 'name'
       spotify.getTopArtists(user, (err, body) => {
         const cur = JSON.parse(body);
         for (var i = 0; i < cur['limit']; i++) {
             userArtists.push(cur['items'][i][out]);
+            for (var j = 0; j < cur['items'][i]['genres'].length; j++) {
+            var genre = cur['items'][i]['genres'][j]
+              if (genres.indexOf(genre) == -1){
+                 genres.push(genre);
+              }
+            }
         }
-        console.log(userArtists);
+        console.log(genres);
 
       });
       spotify.getTopTracks(user, (err, body) => {
@@ -21,6 +28,7 @@ const getValues = (user, done) => {
 
             for (var j = 0; j < cur['items'][i]['album']['artists'].length; j++) {
             var artist = cur['items'][i]['album']['artists'][j][out]
+            //  console.log(cur['items'][i]['album']['artists'][j])
               if (userArtists.indexOf(artist) == -1){
                  userArtists.push(artist);
               }
