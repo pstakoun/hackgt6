0aee84db8cc54ac9a15c72d29ad3f5a9d2e3836c
Peter Stakoun <peter@stakoun.com>
2019-10-26 18:44:42 -0400

    Add group to invites

--- server/routes/invite.js
+++ server/routes/invite.js
@@ -26,6 +26,7 @@ router.post('/', (req, res) => {
         phoneNumber: response.getPhoneNumber(),
         invitedFrom: req.user,
         expiration: response.getExpiration(),
+        group: req.body.group,
       };
       res.status(200).json({ message: response });
     });
